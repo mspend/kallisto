8076925e7fff76522e7188a5f8f664eb91802d51
mspend <mauridobbs@gmail.com>
2026-03-11 18:05:29 -0700

    define all subdirectories using Pathlib; write out the full results and the summary containing only the tpm

--- convert_gene_names.py
+++ convert_gene_names.py
@@ -5,15 +5,18 @@
 
 # Import necessary packages
 import pandas as pd
+from pathlib import Path
+
+data = Path('/data/bulkRNA/IMG/kallisto')
 
 # Read in the RNA counts from Kallisto, stored in abundance.tsv
-counts = '/data/bulkRNA/IMG/kallisto/abundance.tsv'
+counts = data / 'rep_1' / 'abundance.tsv'
 counts_df = pd.read_table(counts, delimiter='\t', header=0)
 
 
 # Read in the file containing the ENSMBL Transcript Identifiers and the gene names
 # T2g = transcripts to genes
-transcripts_to_genes = '/data/bulkRNA/IMG/kallisto/pre_built_index_patcher_lab_t2g.txt'
+transcripts_to_genes = data / 'pre_built_index_patcher_lab_t2g.txt'
 t2g_df = pd.read_table(transcripts_to_genes, header=None, names=["target_id", "gene_id", "gene_name", "transcript_name", "chr", "start", "end", "strand"])
 
 # Add a column to the counts_df on the far right containing the gene name from t2g_df
@@ -22,9 +25,14 @@ counts_merged= counts_df.merge(t2g_df[["target_id", "gene_name"]], on="target_id
 # Quality control: check what percentage of the transcripts are missing a gene name
 print(f'The percent of transcripts whose gene name is N/A is {counts_merged["gene_name"].isna().sum()/len(counts_df)*100}')
 
+# Reorder the columns
+reordered = counts_merged[["gene_name", "target_id", "tpm", "est_counts", "length", "eff_length"]]
+
 # Create a Series containing only the gene name and the TPM
-gene_tpm = counts_merged.groupby("gene_name")["tpm"].sum().reset_index()
+summary = counts_merged.groupby("gene_name")["tpm"].sum().reset_index()
 
 # Save as a CSV
 # Will save to the present working directory
-gene_tpm.to_csv('bulk_RNA_seq_counts_tpm.csv')
+reordered.to_csv(data / 'rep_1' /'bulk_RNA_seq_counts.csv')
+
+summary.to_csv(data / 'rep_1' /'bulk_RNA_seq_counts_summary.csv')
